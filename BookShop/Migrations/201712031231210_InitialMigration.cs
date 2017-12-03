138cc1d250c5286636e974fdce03cd7d4b1597d8
Henrik Christiansen <henpia@gmail.com>
2017-12-03 13:32:21 +0100

    Enabled migrations. Added Initial Migration.

---
+++ BookShop/Migrations/201712031231210_InitialMigration.cs
@@ -0,0 +1,44 @@
+namespace BookShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        BookId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Author = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        GenreId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BookId)
+                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
+                .Index(t => t.GenreId);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        GenreId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.GenreId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Books", new[] { "GenreId" });
+            DropTable("dbo.Genres");
+            DropTable("dbo.Books");
+        }
+    }
+}
