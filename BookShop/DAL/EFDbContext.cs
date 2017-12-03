3b456b12ac486451f87ce0aa069c719caaea790a
Henrik Christiansen <henpia@gmail.com>
2017-12-03 13:27:34 +0100

    Created Genre and Book Entities. Created new EFDbContext in new DAL folder.

---
+++ BookShop/DAL/EFDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookShop.DAL
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("BookShopConnection") { }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookShop/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookShop.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int GenreId { get; set; }
+
+        public virtual Genre Genre { get; set; }
+    }
+}
