e018c50f3a4c003526c09fc296826535869244ea
Henrik Christiansen <henpia@gmail.com>
2017-12-03 16:04:01 +0100

    Seeded example data.

--- BookShop/Migrations/Configuration.cs
+++ BookShop/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace BookShop.Migrations
 {
+    using BookShop.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +20,74 @@ namespace BookShop.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            var genres = new List<Genre>
+            {
+                new Genre
+                {
+                    Name = "Drama",
+                    Description = "Description of the Drama Genre"
+                },
+                new Genre
+                {
+                    Name = "Science Fiction",
+                    Description = "Description of the Science Fiction Genre"
+                },
+                new Genre
+                {
+                    Name = "History",
+                    Description = "Description of the History Genre"
+                }
+            };
+            genres.ForEach(g => context.Genres.AddOrUpdate(p => p.Name, g));
+            context.SaveChanges();
+
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Title = "Romeo and Juliet",
+                    Author = "William Shakespeare",
+                    Price = 15,
+                    GenreId = 1
+                },
+                new Book
+                {
+                    Title = "Macbeth",
+                    Author = "William Shakespeare",
+                    Price = 12,
+                    GenreId = 1
+                },
+                new Book
+                {
+                    Title = "The Hitchhiker's Guide to the Galaxy",
+                    Author = "Douglas Adams",
+                    Price = 18,
+                    GenreId = 2
+                },
+                new Book
+                {
+                    Title = "2001: A Space Odyssey",
+                    Author = "Arthur C. Clarke",
+                    Price = 20,
+                    GenreId = 2
+                },
+                new Book
+                {
+                    Title = "Dune",
+                    Author = "Frank Herbert",
+                    Price = 8,
+                    GenreId = 2
+                },
+                new Book
+                {
+                    Title = "The Decline and Fall of the Roman Empire",
+                    Author = "Edward Gibbon",
+                    Price = 17,
+                    GenreId = 3
+                }
+            };
+            books.ForEach(b => context.Books.AddOrUpdate(p => p.Title, b));
+            context.SaveChanges();
         }
     }
 }
